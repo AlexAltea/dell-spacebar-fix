cf6f68c091c8da2423c58f69a788f11138cafd9d
Alexandro Sanchez Bach <alexandro@phi.nz>
2017-10-21 04:13:53 +0200

    Pushed code

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# dell-spacebar-fix
-Software solution for double-space issues in Dell XPS 13/15 laptops
+# Dell Spacebar Fix
+
+Software solution for double-space issues in Dell XPS 13/15 laptops.
+
+
+## Usage
+
+1. Download the latest snapshot of [dell-spacebar-fix](https://github.com/AlexAltea/dell-spacebar-fix/archive/master.zip).
+
+2. Extract the contents of the .zip file and go to the `bin` folder.
+
+3. Install the service by executing `install-service.bat`.
+
+4. Enjoy!
+
+5. Uninstall the service by executing `uninstall-service.bat`.
+
+
+## Details
+
+Although Dell has seemingly improved this issue, there's still the following occurring pattern where *Ti* represent an increasing sequence of timestamps.
+
+```
+#1  T0  VK_SPACE  WM_KEYDOWN
+#2  T0  VK_SPACE  WM_KEYUP
+#3  T1  *         WM_KEYDOWN
+#4  T1  VK_SPACE  WM_KEYDOWN
+#5  T2  VK_SPACE  WM_KEYUP
+```
+
+This service will detect the event patterns (#1,#2) and (#3,#4), and for each occurrence dropping the events #2 and #4, thus the final events for the above-mentioned sequence will be:
+
+```
+#1  T0  VK_SPACE  WM_KEYDOWN
+#3  T1  *         WM_KEYDOWN
+#5  T2  VK_SPACE  WM_KEYUP
+```
---
+++ src/service.cpp
@@ -0,0 +1,80 @@
+/**
+ * (c) 2017 Alexandro Sanchez Bach
+ * Released under MIT license. Read LICENSE for more details.
+ */
+
+#include <cctype>
+#include <cstdio>
+
+#include <Windows.h>
+
+#ifdef _DEBUG
+#define LOGD(msg, ...) printf(msg "\n", __VA_ARGS__)
+#else
+#define LOGD
+#endif
+
+// Global state
+static WPARAM lastEvent = 0;
+static DWORD lastKey = 0;
+static DWORD lastTime = 0;
+
+bool IsAlphaNumKey(DWORD vkCode)
+{
+	return
+		(vkCode >= 0x30 && vkCode <= 0x39) ||  // [0-9]
+		(vkCode >= 0x41 && vkCode <= 0x5A);    // [A-Z]
+}
+
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    KBDLLHOOKSTRUCT *pKeyBoard = (KBDLLHOOKSTRUCT *)lParam;
+
+	// Process following subset of events
+    if (wParam == WM_KEYDOWN || 
+		wParam == WM_KEYUP)
+	{
+		WPARAM thisEvent = wParam;
+		DWORD thisKey = pKeyBoard->vkCode;
+		DWORD thisTime = pKeyBoard->time;
+
+		if (IsAlphaNumKey(thisKey))
+			LOGD("[%d] Key %02X %s", thisTime, thisKey,
+				(wParam == WM_KEYDOWN ? "pressed" : "released"));
+
+		// Filter out #2 from (#1, #2)
+		if (lastKey == VK_SPACE && lastEvent == WM_KEYDOWN &&
+			thisKey == VK_SPACE && thisEvent == WM_KEYUP &&
+			thisTime == lastTime) {
+			return 1;
+		}
+		// Filter out #4 from (#3, #4)
+		if (lastKey != VK_SPACE && lastEvent == WM_KEYDOWN &&
+			thisKey == VK_SPACE && thisEvent == WM_KEYDOWN &&
+			thisTime == lastTime) {
+			return 1;
+		}
+
+		// Update state
+		lastEvent = thisEvent;
+		lastKey = thisKey;
+		lastTime = thisTime;
+    }
+
+    // Forward the event
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+int main()
+{
+	HINSTANCE instance = GetModuleHandle(NULL);
+	HHOOK hook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, instance, 0);
+
+	MSG msg;
+	while (!GetMessage(&msg, NULL, NULL, NULL)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	UnhookWindowsHookEx(hook);
+    return 0;
+}
